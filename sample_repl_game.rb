63ddb5c62484dd984c820c274edc6ad569b7f7cf
Andrea Cardy <acardy@customink.com>
2018-02-14 11:39:47 -0500

    Updated REPL game

--- sample_repl_game.rb
+++ sample_repl_game.rb
@@ -1,16 +1,15 @@
 puts "Welcome to Batman: Arkham Asylum!"
 puts "What do you want to do?"
-puts "Options: look around, smack a prisoner, run away"
-option = gets.chomp
-puts "This is the option variable: #{option}"
-# case option
-# when "look around"
-#     puts "You see a bunch of angry prisoners and the Joker!"
-# when "smack a prisoner"
-#     puts "Ouch! What did I ever do to you, man?"
-# when "run away"
-#     puts "Batman: \"Bats can't run.\""
-# else
-#     puts "A prisoner punches you in the face."
-#     puts "Prisoner: \"Quit messin' around.\""
-# end
+puts "Options: (l)ook around, (s)mack a prisoner, (r)un away"
+option = gets.chomp.downcase
+case option
+when "l"
+    puts "You see a bunch of angry prisoners and the Joker!"
+when "s"
+    puts "Ouch! What did I ever do to you, man?"
+when "r"
+    puts "Batman: \"Bats can't run.\""
+else
+    puts "A prisoner punches you in the face."
+    puts "Prisoner: \"Quit messin' around.\""
+end
