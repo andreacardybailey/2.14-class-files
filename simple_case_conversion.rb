7c5a6651cac13ce2ec7a80dd110b886d731e47f7
Andrea Cardy <acardy@customink.com>
2018-02-14 11:18:29 -0500

    updated case file

--- simple_case_conversion.rb
+++ simple_case_conversion.rb
@@ -1,11 +1,11 @@
 myVar = 1
 
 case myVar
-  when 2
-    puts "The first condition is true."
-  when 1
-    puts "The second condition is true."
   when 3
+    puts "The first condition is true."
+  when Integer
+    puts "The second condition is true."
+  when 1
     puts "The third condition is true."
   else
    puts "None of the conditions are true."
